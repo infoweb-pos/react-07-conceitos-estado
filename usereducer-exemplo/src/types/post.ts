9c11851d339e19cfe7f254a2913bf41492cadaf7
Leonardo Minora <leonardo.minora@gmail.com>
2026-05-29 16:00:58 +0000

    criado tela com uma lista de publicações

---
+++ usereducer-exemplo/src/types/post.ts
@@ -0,0 +1,19 @@
+export type Post = {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+  tags: string[];
+  reactions: {
+    likes: number;
+    dislikes: number;
+  };
+  views: number;
+};
+
+export type PostsResponse = {
+  posts: Post[];
+  total: number;
+  skip: number;
+  limit: number;
+};
